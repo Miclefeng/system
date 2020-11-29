56d002940c663e9dad856442dd1b2d130611d441
miclefengzss <miclefengzss@163.com>
2020-11-29 21:10:36 +0800

    Initial Commit

---
+++ pointer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  pointer
+//
+//  Created by miclefengzss on 2020/11/29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
